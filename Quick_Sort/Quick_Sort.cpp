a38dcff9bcc2bbbd150b452357011ec742ca2a2c
jack6332 <b10815044@gapps.ntust.edu.tw>
2020-12-31 22:24:20 +0800

    update

---
+++ Quick_Sort/Quick_Sort.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include<string>
+#include<vector>
+
+using namespace std;
+
+void quicksort(vector<string>& list, int, int end);
+
+void partition(vector<string>& list, int front, int end);
+
+int main()
+{
+	string input;
+	vector<string>list;
+
+	while (cin >> input)
+	{
+		list.push_back(input);
+		char ch = getchar();
+		if (ch == '\n')
+			break;
+	}
+	quicksort(list, 0, list.size() - 1);
+	for (int i = 0; i < list.size(); i++) {
+		cout << list[i];
+		if (i != list.size() - 1)
+			cout << " ";
+	}
+}
+
+void quicksort(vector<string>& list, int begin, int end) {
+	if (begin < end) {
+		int pivot = (begin + end) / 2;
+		partition(list, begin, end);
+		quicksort(list, begin, pivot - 1);
+		quicksort(list, pivot + 1, end);
+	}
+}
+void partition(vector<string>& list, int front, int end) {
+	string pivot = list[end];
+	int count = front - 1;
+	for (int j = front; j < end; j++) {
+		if (list[j] < pivot) {
+			swap(list[++count], list[j]);
+		}
+	}
+	count++;
+	swap(list[count], list[end]);
+}
---
+++ Shell Sort/Shell Sort.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int input[] = { 63,19,7,90,81,36,54,45,72,27,22,9,41,59,33 };
+    int size = 15;
+    int flag = 1, gap_size = 15;
+    while (flag || gap_size > 1) {
+        flag = 0;
+        gap_size = (gap_size + 1) / 2;
+        for (int i = 0; i < 15 - gap_size;i++) {
+            if (input[i + gap_size] < input[i]) {
+                swap(input[i + gap_size], input[i]);
+                flag = 1;
+            }
+        }
+    }
+    for (int j = 0; j < 15; j++)
+    {
+        cout << input[j] << " ";
+    }
+    cout << endl;
+}
